98507496107a4f9c3cd280c356f6db06f0d20d6c
Eddie Pelletier <Eddiepelletier.2006@gmail.com>
2025-02-27 21:01:52 -0500

    Repaired code to be compatible with current php version

--- scripts/eventWatcher.php
+++ scripts/eventWatcher.php
@@ -43,7 +43,7 @@ while (true) {
         $current_files = scandir($run_path);
         $new_files = array_diff($current_files, $known_files);
         foreach ($new_files as $file) {
-            if (strpos($file, $variable) !== false && strpos($file, $level) !== false && str_ends_with($file, '.webp')) {
+            if (strpos($file, $variable) !== false && strpos($file, $level) !== false && substr($file, -5) === '.webp') {
                 echo "data: " . json_encode(["forecast" => $file]) . "\n\n";
                 ob_flush();
                 flush();
