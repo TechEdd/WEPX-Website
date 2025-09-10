f0fdb56a7a027e124855f389d00ec09b49989a64
TechEdd <30571076+TechEdd@users.noreply.github.com>
2025-09-10 14:16:29 -0400

    Changed title to accurate name

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multiple Image Pan/Zoom with Touch Support</title>
+    <title>WEPX Weather Toolkit</title>
     <style>
         body {
             margin: 0;
